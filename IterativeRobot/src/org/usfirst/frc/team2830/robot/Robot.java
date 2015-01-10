5dfbf1b9ed2089a129d230ee13da9276b383f922
Erik <ErikSLee@gmail.com>
2015-01-10 17:12:45 -0600

    Add Sample Mecanum code

--- IterativeRobot/src/org/usfirst/frc/team2830/robot/Robot.java
+++ IterativeRobot/src/org/usfirst/frc/team2830/robot/Robot.java
@@ -2,6 +2,10 @@
 package org.usfirst.frc.team2830.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,33 +15,54 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	
+	
+    RobotDrive robotDrive;
+    Joystick stick;
+
+    // Channels for the wheels
+    final int frontLeftChannel	= 2;
+    final int rearLeftChannel	= 3;
+    final int frontRightChannel	= 1;
+    final int rearRightChannel	= 0;
+    
+    // The channel on the driver station that the joystick is connected to
+    final int joystickChannel	= 0;
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+    	robotDrive.setExpiration(0.1);
+        robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
+    	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
+    	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);		// you may need to change or remove this to match your robot
 
+        stick = new Joystick(joystickChannel);
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	//TODO
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getZ(), 0);
         
+        Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+    //TODO
     }
     
 }
